e983ec49378411750d341fe3a4014c5cbc9be0d3
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2022-11-09 21:49:52 -0500

    Initialize react project, set up tailwind css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Pomodoro from "./components/Pomodoro/pomodoro";
+function App() {
+  return (
+    <div>
+      <p>test</p>
+      <Pomodoro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Pomodoro from "./components/Pomodoro/pomodoro";
+import './index.css'
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Pomodoro />
+);
